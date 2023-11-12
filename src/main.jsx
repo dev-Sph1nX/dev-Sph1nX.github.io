20d59a082e5690c94212a65efa8e0ac3edc8e566
Lucas Leperlier <lucasleperlier1010@gmail.com>
2023-11-12 22:39:03 +0100

    wip

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,20 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import { ChakraBaseProvider } from "@chakra-ui/react";
+import { CacheProvider } from "@emotion/react";
+import createCache from "@emotion/cache";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const myCache = createCache({
+  key: 'my-prefix-key',
+})
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+      <CacheProvider value={myCache}>
+        <ChakraBaseProvider >
+            <App />
+        </ChakraBaseProvider>
+      </CacheProvider>
+  </React.StrictMode>
+);
